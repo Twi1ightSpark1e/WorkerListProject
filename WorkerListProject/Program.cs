988ad76e65c3de46a109b3b61f3fd28969fb3313
Twilight_Sparkle <inj3ct0rpro@gmail.com>
2019-03-14 16:25:03 +0300

    Нормализация документации в файле и комментариев в методе вывода фильтрованного списка

--- WorkerListProject/Program.cs
+++ WorkerListProject/Program.cs
@@ -15,40 +15,15 @@ namespace WorkerListProject
     /// Работник
     /// </summary>
     struct Worker {
-        /// <summary>
-        /// Фамилия работника
-        /// Значение должно быть непустым
-        /// </summary>
-        string fullName;
+        string fullName;        // Фамилия работника
+        string position;        // Должность работника
+        DateTime signingDate;   // Дата подписания контракта
+        uint contractDuration;  // Срок действия контракта
+        float salary;           // Оклад
 
         /// <summary>
-        /// Должность работника
-        /// Значение должно быть непустым
+        /// Вывод текущего работника
         /// </summary>
-        string position;
-
-        /// <summary>
-        /// Дата подписания контракта
-        /// Значение должно быть непустым
-        /// </summary>
-        DateTime signingDate;
-
-        /// <summary>
-        /// Срок действия контракта
-        /// Значение должно быть больше 0
-        /// </summary>
-        uint contractDuration;
-
-        /// <summary>
-        /// Оклад
-        /// Значение должно быть неотрицательным
-        /// </summary>
-        float salary;
-
-        /// <summary>
-        /// Вывод одного работника
-        /// </summary>
-        /// <param type="Worker">Работник</param>
         public void Print() {
             Console.WriteLine($"fullName: {fullName} position: {position}" +
                 $" signingDate: {signingDate} contractDuration: {contractDuration}" +
@@ -58,7 +33,7 @@ namespace WorkerListProject
         /// <summary>
         /// Вывод всех работников
         /// </summary>
-        /// <param type="List<Worker>">Список работников</param>
+        /// <param name="listwrks">Список работников</param>
         public static void Print(List<Worker> listwrks)
         {
             foreach (Worker wrk in listwrks)
@@ -68,9 +43,66 @@ namespace WorkerListProject
         }
 
         /// <summary>
+        /// Вывод фильтрованного списка структуры
+        /// </summary>
+        /// <param name="workers">Список работников</param>
+        /// <param name="filter">Значения фильтра</param>
+        public static void PrintFiltered(List<Worker> workers, Filter filter)
+        {
+            foreach (var worker in workers) // цикличная проверка каждого рабочего
+            {
+                bool shouldPrint = true; // флаг, означающий нужно ли выводить данного рабочего
+
+                // Проверка: включает ли ФИО рабочего соответствующую строку из фильтра
+                if ((filter.fullNameSubstring != null) &&
+                    (!worker.fullName.Contains(filter.fullNameSubstring)))
+                {
+                    shouldPrint = false;
+                }
+
+                // Проверка: включает ли должность работника соответствующую строку из фильтра
+                if ((filter.positionSubstring != null) &&
+                    (!worker.position.Contains(filter.positionSubstring)))
+                {
+                    shouldPrint = false;
+                }
+
+                // Проверка: принадлежит ли дата подписания контракта
+                //           соответствующему диапазону из фильтра
+                if ((worker.signingDate < filter.signingDateLowerBound) ||
+                    (worker.signingDate > filter.signingDateUpperBound))
+                {
+                    shouldPrint = false;
+                }
+
+                // Проверка: принадлежит ли срок действия контракта
+                //           соответствующему диапазону из фильтра
+                if ((worker.contractDuration < filter.contractDurationLowerBound) ||
+                    (worker.contractDuration > filter.contractDurationUpperBound))
+                {
+                    shouldPrint = false;
+                }
+
+                // Проверка: принадлежит ли оклад работника
+                //           соответствующему диапазону из фильтра
+                if ((worker.salary < filter.salaryLowerBound) ||
+                    (worker.salary > filter.salaryUpperBound))
+                {
+                    shouldPrint = false;
+                }
+
+                // Если данный рабочий прошёл все проверки — вывести его
+                if (shouldPrint)
+                {
+                    worker.Print();
+                }
+            }
+        }
+
+        /// <summary>
         /// Добавление одного элемента структуры в список работников
         /// </summary>
-        /// <param type="List<Worker>">Список работников</param>
+        /// <param name="workers">Список работников</param>
         public static Worker AddWorker(List<Worker> workers)
         {
             Worker worker = new Worker();
@@ -115,60 +147,20 @@ namespace WorkerListProject
             workers.Add(worker);
             return worker;
         }
-
-        /// <summary>
-        /// Вывод фильтрованного списка структуры
-        /// </summary>
-        public static void PrintFiltered(List<Worker> workers, Filter filter)
-        {
-            foreach (var worker in workers)
-            {
-                bool shouldPrint = true;
-
-                if ((filter.fullNameSubstring != null) &&
-                    (!worker.fullName.Contains(filter.fullNameSubstring)))
-                {
-                    shouldPrint = false;
-                }
-
-                if ((filter.positionSubstring != null) &&
-                    (!worker.position.Contains(filter.positionSubstring)))
-                {
-                    shouldPrint = false;
-                }
-
-                if ((worker.signingDate < filter.signingDateLowerBound) ||
-                    (worker.signingDate > filter.signingDateUpperBound))
-                {
-                    shouldPrint = false;
-                }
-
-                if ((worker.contractDuration < filter.contractDurationLowerBound) ||
-                    (worker.contractDuration > filter.contractDurationUpperBound))
-                {
-                    shouldPrint = false;
-                }
-
-                if ((worker.salary < filter.salaryLowerBound) ||
-                    (worker.salary > filter.salaryUpperBound))
-                {
-                    shouldPrint = false;
-                }
-
-                if (shouldPrint)
-                {
-                    worker.Print();
-                }
-            }
-        }
     }
 
+    /// <summary>
+    /// Фильтр
+    /// </summary>
     struct Filter
     {
-        public string fullNameSubstring;
-        public string positionSubstring;
-        public DateTime? signingDateLowerBound, signingDateUpperBound;
-        public uint? contractDurationLowerBound, contractDurationUpperBound;
-        public float? salaryLowerBound, salaryUpperBound;
+        public string fullNameSubstring;            // Подстрока, которая должна быть в ФИО
+        public string positionSubstring;            // Подстрока, которая должна быть в должности
+        public DateTime? signingDateLowerBound;     // Нижняя граница диапазона даты подписания
+        public DateTime? signingDateUpperBound;     // Верхняя граница диапазона даты подписания
+        public uint? contractDurationLowerBound;    // Нижняя граница диапазона срока действия контракта
+        public uint? contractDurationUpperBound;    // Верхняя граница диапазона срока действия контракта
+        public float? salaryLowerBound;             // Нижняя граница диапазона оклада
+        public float? salaryUpperBound;             // Верхняя граница диапазона оклада
     }
 }
